596d30a225dee5ab059684f3be31b25839d9b244
Brooke Amaranth <brookea@sidefx.com>
2025-01-09 15:50:24 -0500

    Houdini Engine for 3ds Max: fixed bug that could cause incorrect transforms on output geometry. This was caused by passing a Point3 instead of a ScaleValue to the scale controller's GetValue method.

--- src/HEMAX_Utilities.cpp
+++ src/HEMAX_Utilities.cpp
@@ -150,7 +150,7 @@ HEMAX_Utilities::BuildMaxTransformFromINode(INode* Node)
     Node->GetTMController()->GetRotationController()->GetValue(
         Time, &Rotation, Forever);
 
-    Point3 Scale;
+    ScaleValue Scale;
     Node->GetTMController()->GetScaleController()->GetValue(
         Time, &Scale, Forever);
 
@@ -165,9 +165,9 @@ HEMAX_Utilities::BuildMaxTransformFromINode(INode* Node)
     MaxTransform.Quaternion[2] = Rotation.z;
     MaxTransform.Quaternion[3] = Rotation.w;
 
-    MaxTransform.Scale[0] = Scale.x;
-    MaxTransform.Scale[1] = Scale.y;
-    MaxTransform.Scale[2] = Scale.z;
+    MaxTransform.Scale[0] = Scale[0];
+    MaxTransform.Scale[1] = Scale[1];
+    MaxTransform.Scale[2] = Scale[2];
 
     return MaxTransform;
 }
